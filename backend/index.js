d56f70ad4fa51dc2954157c84823074537db6300
unknown <itsnitu028@gmail.com>
2025-04-25 02:50:35 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv").config()
+const connectDb=require("./config/connectionDB")
+const cors=require("cors")
+
+const PORT=process.env.PORT || 3000
+connectDb()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+// app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.use("/",require("./routes/user"))
+app.use("/recipe",require("./routes/recipe"))
+
+
+app.listen(PORT,(err)=>{
+    console.log(`app is listening on port ${PORT}`)
+})
